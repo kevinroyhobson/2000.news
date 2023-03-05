a807b5263f2e84af9541d2dc7d771487b0cc8f5f
Kevin Hobson <kevinroyhobson@gmail.com>
2023-03-05 14:35:01 -0500

    Improve content by restricting to specific sources.

--- backend/Fetch/fetch.py
+++ backend/Fetch/fetch.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+import random
 import boto3
 from botocore.exceptions import ClientError
 import requests
@@ -7,27 +8,71 @@ import requests
 
 _news_data_api_key = os.environ['NEWS_DATA_API_KEY']
 _news_data_endpoint = "https://newsdata.io/api/1/news"
-_categories = ["politics", "sports", "technology", "top", "world"]
+_sources = {"abcnews",
+            "bloomberg",
+            "deadline",
+            "financialtimes",
+            "foxnews",
+            "huffpost",
+            "kron4",
+            "latimes",
+            "nasa",
+            "nbcnews",
+            "news-medical",
+            "newyorkcbslocal",
+            "nypost",
+            "nytimes",
+            "si",
+            "skysports",
+            "theringer",
+            "tmz",
+            "usnews",
+            "venturebeat",
+            "washingtonpost",
+            "wsj"}
 
 _dynamo_resource = boto3.resource('dynamodb')
 _stories_table = _dynamo_resource.Table('Stories')
 
 
 def fetch(event, context):
-    stories_response = fetch_stories()
-    num_stories_saved = 0
-    for story in stories_response['results']:
-        print(f"Processing story '{story['title']}'")
-        if save_story(story):
-            num_stories_saved += 1
+    this_instance_sources = random.sample(_sources, 5)
+    stories_response = fetch_stories(this_instance_sources)
 
-    human_readable_result = f"Processed {len(stories_response['results'])} stories of {stories_response['totalResults']} total stories. Saved {num_stories_saved} stories."
+    num_stories_saved = 0
+    num_stories_processed = 0
+    num_api_calls = 1
+
+    while True:
+        for story in stories_response['results']:
+            print(f"Processing story '{story['title']}' ({story['source_id']})")
+            num_stories_processed += 1
+            if save_story(story):
+                num_stories_saved += 1
+
+        if num_stories_saved >= 20:
+            break
+
+        if 'nextPage' not in stories_response or stories_response['nextPage'] is None:
+            break
+
+        if num_api_calls >= 4:
+            break
+
+        stories_response = fetch_stories(this_instance_sources, stories_response['nextPage'])
+        num_api_calls += 1
+
+    human_readable_result = f"Processed {num_stories_processed} stories of {stories_response['totalResults']} total stories. Saved {num_stories_saved} stories."
     print(human_readable_result)
     return human_readable_result
 
 
-def fetch_stories():
-    query_params = f"country=us&language=en&category={','.join(_categories)}&apikey={_news_data_api_key}"
+def fetch_stories(sources, page_token=None):
+    query_params = f"country=us&language=en&domain={','.join(sources)}&apikey={_news_data_api_key}"
+    if page_token is not None:
+        query_params += f"&page={page_token}"
+
+    print(f"Fetching stories with query params: {query_params}")
     response = requests.get(f"{_news_data_endpoint}?{query_params}").json()
 
     if response['status'] == 'error':
@@ -40,6 +85,10 @@ def fetch_stories():
 
 
 def save_story(story):
+    if 'source_id' not in story or story['source_id'] not in _sources:
+        print(f"Skipped story '{story['title']}' because it has no source.")
+        return False
+
     try:
         _stories_table.put_item(
             Item={
@@ -56,6 +105,7 @@ def save_story(story):
                 'Country': story['country'],
                 'Keywords': story['keywords'],
                 'Category': story['category'],
+                'Source': story['source_id'],
                 'RetrievedTime': datetime.datetime.now().isoformat()
             },
             ConditionExpression="attribute_not_exists(YearMonthDay) AND attribute_not_exists(Title)"
@@ -64,7 +114,7 @@ def save_story(story):
 
     except ClientError as ex:
         if ex.response['Error']['Code'] == 'ConditionalCheckFailedException':
-            print(f"Skipped story '{story['title']}' because it already exists.")
+            print(f"Skipped story '{story['title']} ({story['source_id']}' because it already exists.")
         else:
             raise ex
 
